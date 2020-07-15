da754c72b98fe52696d195335cce8af48daa2dcb
ChenHang <237113730@qq.com>
2020-07-15 10:09:29 +0800

    新闻模块-图片

--- src/main/java/com/rtc/manager/service/impl/NewsImpl.java
+++ src/main/java/com/rtc/manager/service/impl/NewsImpl.java
@@ -9,6 +9,7 @@ import com.rtc.manager.vo.RtcNewsDetatilVO;
 import com.rtc.manager.vo.RtcNewsVO;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -34,6 +35,9 @@ public class NewsImpl implements News {
     @Autowired
     private RtcNewsDetailMapper rtcNewsDetailMapper;
 
+    @Value("${rtc.url}")
+    private String url;
+
     @SneakyThrows
     @Override
     public PageInfo<RtcNewsVO> listNews(String startDate, String endDate, int pageNum, int pageSize, Integer sequence) {
@@ -47,8 +51,8 @@ public class NewsImpl implements News {
                     rtcNewsVO.setDescription(rtcNewsVO.getDescription().replaceAll("<[^>]*>", ""));
                     String url = rtcNewsVO.getPreview();
                     if (null != url) {
-                        url = "https://192.168.1.125" + url.substring(24);
-                        url = "http://192.168.1.125/chinadaily/2020-07/14/1594716726-1799.jpeg";
+                        url = this.url + url.substring(24);
+//                        url = this.url + "/chinadaily/2020-07/14/1594716726-1799.jpeg";
                         BufferedImage sourceImg=ImageIO.read(new URL(url).openStream());
                         // 单位：像素
                         int width = sourceImg.getWidth();
@@ -81,16 +85,18 @@ public class NewsImpl implements News {
                     if (p.contains("figcaption") || p.contains("<img")) {
                         String url = p.substring(p.indexOf("'") + 1, p.indexOf("'", p.indexOf("'") + 1));
 //                        url = "http://192.168.1.125/" + url.substring(7);
+                        url = this.url + url.substring(24);
                         File picture = new File(url);
-                        BufferedImage sourceImg = ImageIO.read(new FileInputStream(picture));
+//                        BufferedImage sourceImg = ImageIO.read(new FileInputStream(picture));
+                        BufferedImage sourceImg=ImageIO.read(new URL(url).openStream());
                         // 单位：像素
                         int width = sourceImg.getWidth();
                         int height = sourceImg.getHeight();
                         map.put("data", p.replaceAll("<[^>]*>", ""));
                         map.put("type", "img");
                         map.put("url", url);
-//                        map.put("width", width);
-//                        map.put("height", height);
+                        map.put("width", width);
+                        map.put("height", height);
                         resultList.add(map);
                     } else if (p.contains("<strong>")) {
                         map.put("data", p.replaceAll("<[^>]*>", ""));
