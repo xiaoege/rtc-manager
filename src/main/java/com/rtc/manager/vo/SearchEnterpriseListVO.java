29b5e7773fa1e8ee82f9a383cadfa1056ee72a72
ChenHang <237113730@qq.com>
2021-01-14 13:59:05 +0800

    修改了es-企业搜索列表返回字段

--- src/main/java/com/rtc/manager/vo/SearchEnterpriseListVO.java
+++ src/main/java/com/rtc/manager/vo/SearchEnterpriseListVO.java
@@ -17,72 +17,19 @@ public class SearchEnterpriseListVO {
     private String eName;
 
     private String nation;
+    /**
+     * mysql表的id
+     */
     private Integer pid;
+
+    /**
+     * es-document的id
+     */
+    private String esId;
+
     @JsonAlias("enterprise_id")
     private String enterpriseId;
-    // india-cin
-    /*private String cin;
-    @JsonAlias("roc_code")
-    private String rocCode;
-    @JsonAlias("registration_number")
-    private String registrationNumber;
-    @JsonAlias("company_category")
-    private String companyCategory;
-    @JsonAlias("company_sub_category")
-    private String companySubCategory;
-    @JsonAlias("class_of_company")
-    private String classOfCompany;
-    @JsonAlias("authorised_capital")
-    private String authorisedCapital;
-    @JsonAlias("paid_up_capital")
-    private String paidUpCapital;
-    @JsonAlias("number_of_members")
-    private String numberOfMembers;
-    @JsonAlias("date_of_incorporation")
-    private String dateOfIncorporation;
-    @JsonAlias("email_id")
-    private String emailId;
-    @JsonAlias("whether_listed_or_not")
-    private String whetherListedOrNot;
-    @JsonAlias("active_compliant")
-    private String activeCompliant;
-    @JsonAlias("suspended_at_stock_exchange")
-    private String suspendedAtStockExchange;
-    @JsonAlias("date_of_last_agm")
-    private String dateOfLastAgm;
-    @JsonAlias("date_of_balance_sheet")
-    private String dateOfBalanceSheet;
-    @JsonAlias("company_status")
-    private String companyStatus;
-    @JsonAlias("registered_address")
-    private String registeredAddress;*/
 
-    // india-llpin
-    /*private String llpin;
-    @JsonAlias("number_of_partners")
-    private String numberOfPartners;
-    @JsonAlias("number_of_designated_partners")
-    private String numberOfDesignatedPartners;
-    @JsonAlias("registrated_address")
-    private String registratedAddress;
-    @JsonAlias("previous_firm")
-    private String previousFirm;
-    @JsonAlias("total_obligation_of_contribution")
-    private String totalObligationOfContribution;
-    @JsonAlias("main_division_of_business")
-    private String mainDivisionOfBusiness;
-    @JsonAlias("description_of_main_division")
-    private String descriptionOfMainDivision;
-    @JsonAlias("statement_date")
-    private String statementDate;
-    @JsonAlias("annual_date")
-    private String annualDate;
-    @JsonAlias("llp_status")
-    private String llpStatus;*/
-
-    // China
-//    @JsonAlias("registered_capital")
-//    private String registeredCapital;
     /**
      * 法人代表
      */
@@ -98,13 +45,17 @@ public class SearchEnterpriseListVO {
      */
     @JsonAlias({"registered_address", "registrated_address", "principal_address"})
     private String address;
-//    @JsonAlias("country_region")
-//    private String countryRegion;
 
+    /**
+     * es 文档创建时间
+     */
+    @JsonAlias("@timestamp")
+    private String createTime;
 
     public String geteType() {
         return eType;
     }
+
     @JsonSetter("e_type")
     public void seteType(String eType) {
         this.eType = eType;
@@ -113,6 +64,7 @@ public class SearchEnterpriseListVO {
     public String geteName() {
         return eName;
     }
+
     @JsonSetter("e_name")
     public void seteName(String eName) {
         this.eName = eName;
